44745e79bfb7922314bc7cd5fce22c2338c22acf
Samia <samiamdin2000@gmail.com>
2025-06-15 14:32:33 +0500

    fix: env and backend API integration

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://excursion-backend.onrender.com", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
